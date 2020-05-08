12b6d687ff46be3da6365aef58325b392495e30c
Nacho-C <nachocasamayou@gmail.com>
2020-05-08 17:39:42 -0300

    Correcciones al traductor

--- Traductor/main.c
+++ Traductor/main.c
@@ -9,17 +9,19 @@ typedef struct
     char mnemonico[5], arg1[10], arg2[10];
 } TInstruccionTexto;
 
-void generarTraduccion(char* instrucciones[], int* contInstrucciones);
+void generarTraduccion(char* mnemonicos[], int* contMnemonicos, char* indiceRegistros[]);
 
-void PrimeraPasada(FILE* archEnt, TRotulo rotulos[], int* contRotulos, TInstruccionTexto instrucciones[], int* contLinea);
+void primeraPasada(FILE* archEnt, TRotulo rotulos[], int* contRotulos, TInstruccionTexto instrucciones[], int* contLinea);
 
-void MostrarRotulos(int cant, TRotulo rotulos[]);
+void mostrarRotulos(TRotulo rotulos[], int cant);
 
-void argumentoGenerico(TRam ram, int i, char argumento[], TRotulo rotulos[], int contRotulos, int* huboError, int numArg);
+long traducirMnemonico(char* mnemonicos[], int contMnemonicos, char arg[]);
 
-long traducirMnemonico(char* instrucciones[], int contInstrucciones, char mnemonico[]);
+void argumentoGenerico(TRam ram, int i, char argumento[], TRotulo rotulos[], int contRotulos, char* indiceRegistros[], int* huboError, int numArg);
 
 int traducirRotulo(TRotulo rotulos[], int contRotulos, char rotulo[]);
+
+int traducirRegistro(char* indiceRegistros[], char arg[]);
 
 int main(int argc, char *argv[])
 {
@@ -27,20 +29,22 @@ int main(int argc, char *argv[])
     TRam ram = {0};
     TRotulo rotulos[100];
     TInstruccionTexto instruccionesTexto[100];
-    char* instrucciones[255];
+    char* mnemonicos[255];
+    char* indiceRegistros[16];
     long codMaq;
-    int contRotulos, contLinea, contInstrucciones, i = 0, huboError = 0, output = 1;
+    int contRotulos, contLinea, contMnemonicos, i = 0, huboError = 0, output = 1;
     char aux[4], linea[100];
     FILE* archEnt;
     FILE* archSalida;
     archEnt = fopen(argv[1],"rt");
     if (archEnt == NULL)
     {
-        printf("No se encontro el archivo de entrada.\n");
+        printf("ERROR: No se encontro el archivo de entrada.\n");
         return 1;
     }
-    generarTraduccion(instrucciones, &contInstrucciones);
-    PrimeraPasada(archEnt,rotulos,&contRotulos,instruccionesTexto,&contLinea);
+    generarTraduccion(mnemonicos, &contMnemonicos, indiceRegistros);
+    primeraPasada(archEnt,rotulos,&contRotulos,instruccionesTexto,&contLinea);
+    //mostrarRotulos(rotulos,contRotulos); //COMENTAR ESTO
     registros[2] = contLinea * 3;
     registros[3] = 1000;
     if(argc>3 && strcmp("-o",argv[3])==0)
@@ -48,19 +52,19 @@ int main(int argc, char *argv[])
     while (i < contLinea)
     {
         strcpy(aux,"   ");
-        codMaq = traducirMnemonico(instrucciones,contInstrucciones,instruccionesTexto[i].mnemonico);
+        codMaq = traducirMnemonico(mnemonicos,contMnemonicos,instruccionesTexto[i].mnemonico);
         if (codMaq == -1)
         {
             huboError = 1;
             ram[i*3] = ram[i*3+1] = ram[i*3+2] = 0xFFFFFFFF;
-            printf("No se encontro el mnemonico en la linea %d\n",i+1);
+            printf("ERROR: No se encontro el mnemonico en la linea %d\n",i+1);
         }
         else
         {
-            if (strcmp(instruccionesTexto[i].mnemonico,"STOP") == 0)
+            if (codMaq == 0x8f)
                 ram[i*3] = codMaq << 16;
             else
-                if (strcmp(instruccionesTexto[i].mnemonico,"SYS") == 0)
+                if (codMaq == 0x81)
                 {
                     ram[i*3] = codMaq << 16;
                     ram[i*3+1] = atoi(instruccionesTexto[i].arg1);
@@ -68,9 +72,9 @@ int main(int argc, char *argv[])
                 else
                 {
                     ram[i*3] = (codMaq << 8);
-                    argumentoGenerico(ram,i,instruccionesTexto[i].arg1,rotulos,contRotulos,&huboError,1);
-                    if (strcmp(instruccionesTexto[i].mnemonico,"NOT") != 0 && (instruccionesTexto[i].mnemonico[0] != 'J' || (instruccionesTexto[i].mnemonico[0] == 'J' && (instruccionesTexto[i].mnemonico[1] == 'E' || instruccionesTexto[i].mnemonico[1] == 'G' || instruccionesTexto[i].mnemonico[1] == 'L'))))
-                        argumentoGenerico(ram,i,instruccionesTexto[i].arg2,rotulos,contRotulos,&huboError,2);
+                    argumentoGenerico(ram,i,instruccionesTexto[i].arg1,rotulos,contRotulos,indiceRegistros,&huboError,1);
+                    if (codMaq != 0x33 && codMaq != 0x20 && (codMaq < 24 || codMaq > 29))
+                        argumentoGenerico(ram,i,instruccionesTexto[i].arg2,rotulos,contRotulos,indiceRegistros,&huboError,2);
                 }
         }
         i++;
@@ -81,9 +85,9 @@ int main(int argc, char *argv[])
         rewind(archEnt);
         for (i=0; i<contLinea; i++)
         {
-            linea[0] = '\n';
-            while (linea[0] == '\n' || linea[0] == '/')
+            do
                 fgets(linea,100,archEnt);
+            while (linea[0] == '\n' || linea[0] == '/');
             printf("\n[%08X]\t\t\t%08X %08X %08X\t%s",i*3,ram[i*3],ram[i*3+1],ram[i*3+2],linea);
         }
     }
@@ -99,45 +103,65 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void generarTraduccion(char* instrucciones[], int* contInstrucciones)
+void generarTraduccion(char* mnemonicos[], int* contMnemonicos, char* indiceRegistros[])
 {
     int i;
-    *contInstrucciones = 144;
+
+    *contMnemonicos = 144;
     for (i=0; i<144; i++)
     {
-        instrucciones[i] = (char*) malloc(5*sizeof(char));
-        strcpy(instrucciones[i],"");
+        mnemonicos[i] = (char*) malloc(5*sizeof(char));
+        strcpy(mnemonicos[i],"");
     }
-    strcpy(instrucciones[0x01],"MOV");
-    strcpy(instrucciones[0x02],"ADD");
-    strcpy(instrucciones[0x03],"SUB");
-    strcpy(instrucciones[0x04],"MUL");
-    strcpy(instrucciones[0x05],"DIV");
-    strcpy(instrucciones[0x06],"MOD");
-    strcpy(instrucciones[0x13],"CMP");
-    strcpy(instrucciones[0x17],"SWAP");
-    strcpy(instrucciones[0x19],"RND");
-    strcpy(instrucciones[0x31],"AND");
-    strcpy(instrucciones[0x32],"OR");
-    strcpy(instrucciones[0x33],"NOT");
-    strcpy(instrucciones[0x34],"XOR");
-    strcpy(instrucciones[0x37],"SHL");
-    strcpy(instrucciones[0x38],"SHR");
-    strcpy(instrucciones[0x20],"JMP");
-    strcpy(instrucciones[0x21],"JE");
-    strcpy(instrucciones[0x22],"JG");
-    strcpy(instrucciones[0x23],"JL");
-    strcpy(instrucciones[0x24],"JZ");
-    strcpy(instrucciones[0x25],"JP");
-    strcpy(instrucciones[0x26],"JN");
-    strcpy(instrucciones[0x27],"JNZ");
-    strcpy(instrucciones[0x28],"JNP");
-    strcpy(instrucciones[0x29],"JNN");
-    strcpy(instrucciones[0x81],"SYS");
-    strcpy(instrucciones[0x8F],"STOP");
+    strcpy(mnemonicos[0x01],"MOV");
+    strcpy(mnemonicos[0x02],"ADD");
+    strcpy(mnemonicos[0x03],"SUB");
+    strcpy(mnemonicos[0x04],"MUL");
+    strcpy(mnemonicos[0x05],"DIV");
+    strcpy(mnemonicos[0x06],"MOD");
+    strcpy(mnemonicos[0x13],"CMP");
+    strcpy(mnemonicos[0x17],"SWAP");
+    strcpy(mnemonicos[0x19],"RND");
+    strcpy(mnemonicos[0x31],"AND");
+    strcpy(mnemonicos[0x32],"OR");
+    strcpy(mnemonicos[0x33],"NOT");
+    strcpy(mnemonicos[0x34],"XOR");
+    strcpy(mnemonicos[0x37],"SHL");
+    strcpy(mnemonicos[0x38],"SHR");
+    strcpy(mnemonicos[0x20],"JMP");
+    strcpy(mnemonicos[0x21],"JE");
+    strcpy(mnemonicos[0x22],"JG");
+    strcpy(mnemonicos[0x23],"JL");
+    strcpy(mnemonicos[0x24],"JZ");
+    strcpy(mnemonicos[0x25],"JP");
+    strcpy(mnemonicos[0x26],"JN");
+    strcpy(mnemonicos[0x27],"JNZ");
+    strcpy(mnemonicos[0x28],"JNP");
+    strcpy(mnemonicos[0x29],"JNN");
+    strcpy(mnemonicos[0x81],"SYS");
+    strcpy(mnemonicos[0x8F],"STOP");
+
+    for (i=0; i<16; i++)
+        indiceRegistros[i] = (char*) malloc(5*sizeof(char));
+    strcpy(indiceRegistros[0],"PS");
+    strcpy(indiceRegistros[1],"CS");
+    strcpy(indiceRegistros[2],"DS");
+    strcpy(indiceRegistros[3],"ES");
+    strcpy(indiceRegistros[4],"IP");
+    strcpy(indiceRegistros[5],"SS");
+    strcpy(indiceRegistros[6],"SP");
+    strcpy(indiceRegistros[7],"BP");
+    strcpy(indiceRegistros[8],"AC");
+    strcpy(indiceRegistros[9],"CC");
+    strcpy(indiceRegistros[10],"AX");
+    strcpy(indiceRegistros[11],"BX");
+    strcpy(indiceRegistros[12],"CX");
+    strcpy(indiceRegistros[13],"DX");
+    strcpy(indiceRegistros[14],"EX");
+    strcpy(indiceRegistros[15],"FX");
 }
 
-void PrimeraPasada(FILE* archEnt, TRotulo rotulos[], int* contRotulos,  TInstruccionTexto instrucciones[], int* contLinea)
+void primeraPasada(FILE* archEnt, TRotulo rotulos[], int* contRotulos,  TInstruccionTexto instrucciones[], int* contLinea)
 {
     char linea[100], aux[100], *token;
     (*contLinea) = (*contRotulos) = 0;
@@ -147,27 +171,26 @@ void PrimeraPasada(FILE* archEnt, TRotulo rotulos[], int* contRotulos,  TInstruc
         {
             (*contLinea)++;
             strcpy(aux,linea);
-            token = strtok(linea," \t\n,");
+            token = strtok(linea," \t\n,/");
             if (token[strlen(token)-1] == ':')
             {
                 token[strlen(token)-1] = '\0';
                 rotulos[*contRotulos].linea = (*contLinea);
                 strcpy(rotulos[*contRotulos].rotulo,strupr(token));
                 (*contRotulos)++;
-                token = strtok(NULL,"\n\t ,/");
+                token = strtok(NULL," \t\n,/");
             }
             if (token != NULL)
             {
-
                 strcpy(instrucciones[(*contLinea)-1].mnemonico,strupr(token));
-                token = strtok(NULL,"\n\t ,/");
+                token = strtok(NULL," \t\n,/");
                 if (token != NULL)
                 {
                     if (token[0] == '\'')
                         strcpy(instrucciones[(*contLinea)-1].arg1,token);
                     else
                         strcpy(instrucciones[(*contLinea)-1].arg1,strupr(token));
-                    token = strtok(NULL,"\n\t ,/");
+                    token = strtok(NULL," \t\n,/");
                     if (token != NULL)
                         if (token[0] == '\'')
                             strcpy(instrucciones[(*contLinea)-1].arg2,token);
@@ -175,24 +198,36 @@ void PrimeraPasada(FILE* archEnt, TRotulo rotulos[], int* contRotulos,  TInstruc
                             strcpy(instrucciones[(*contLinea)-1].arg2,strupr(token));
                 }
             }
+            //COMENTAR ESTO
             //printf("\n|%s|%s|%s|\n",instrucciones[(*contLinea)-1].mnemonico,instrucciones[(*contLinea)-1].arg1,instrucciones[(*contLinea)-1].arg2); //Muestra los componentes de la instruccion de cada linea
         }
     }
 }
 
 
-void MostrarRotulos(int cant, TRotulo rotulos[])
+void mostrarRotulos(TRotulo rotulos[], int cant)
 {
     int i;
     for (i=0; i<cant; i++)
         printf("Linea: %d\tRotulo: %s\n", rotulos[i].linea,rotulos[i].rotulo);
 }
 
-void argumentoGenerico(TRam ram, int i, char argumento[], TRotulo rotulos[], int contRotulos, int* huboError, int numArg)
+long traducirMnemonico(char* mnemonicos[], int contMnemonicos, char arg[])
+{
+    int i = 0;
+    while (i < contMnemonicos && strcmp(arg,mnemonicos[i]) != 0)
+        i++;
+    if (i < contMnemonicos)
+        return i;
+    else
+        return -1;
+}
+
+void argumentoGenerico(TRam ram, int i, char argumento[], TRotulo rotulos[], int contRotulos, char* indiceRegistros[], int* huboError, int numArg)
 {
     char aux[4], car;
     int k = 0, j;
-    if (argumento[0] == '[')
+    if (argumento[0] == '[') //Es direccion de memoria
     {
         ram[i*3] += 2;
         if (argumento[1] == 'E')
@@ -222,83 +257,71 @@ void argumentoGenerico(TRam ram, int i, char argumento[], TRotulo rotulos[], int
     }
     else
     {
-        if (argumento[0] >= 65 && strlen(argumento) > 2)
+        if (argumento[0] >= 65 && argumento[0] <= 90)
         {
-            ram[i*3+numArg] = traducirRotulo(rotulos,contRotulos,argumento);
-            if (ram[i*3+numArg] == 0xFFFFFFFF)
+            if (strlen(argumento) == 2) //Es registro
             {
-                *huboError = 1;
-                printf("No se encontro el rotulo %s\n\t\t\t\t",argumento);
-            }
-
-        }
-        else
-            if (argumento[1] == 'C')
-            {
-                ram[i*3] +=1;
-                ram[i*3+numArg] = argumento[0] - 55;
-            }
-            else
-                if (argumento[1] == 'X')
+                ram[i*3] += 1;
+                ram[i*3+numArg] = traducirRegistro(indiceRegistros,argumento);
+                if (ram[i*3+numArg] == 0xFFFFFFFF)
                 {
-                    ram[i*3] +=1;
-                    ram[i*3+numArg] = argumento[0] - 55;
+                    *huboError = 1;
+                    printf("ERROR: No se encontro el registro %s\n\t\t\t\t",argumento);
+                }
+            }
+            else //Es directo, rotulo
+            {
+                ram[i*3+numArg] = traducirRotulo(rotulos,contRotulos,argumento);
+                if (ram[i*3+numArg] == 0xFFFFFFFF)
+                {
+                    *huboError = 1;
+                    printf("ERROR: No se encontro el rotulo %s\n\t\t\t\t",argumento);
+                }
+            }
+        }
+        else //Es directo, no rotulo
+        {
+            j = 1;
+            if  (argumento[0] >= '0' && argumento[0] <= '9') //Es decimal
+               ram[i*3+numArg] = atoi(argumento);
+            else
+                if (argumento[0] == '#') //Es decimal
+                {
+                    argumento[0] = '0';
+                    ram[i*3+numArg] = atoi(argumento);
                 }
                 else
-                    if (argumento[0] == 'A')
-                    {
-                        ram[i*3] += 1;
-                        ram[i*3+numArg] = 8;
-                    }
+                    if (argumento[0] == '\'') //Es caracter
+                        ram[i*3+numArg] = argumento[1];
                     else
-                        if (argumento[0] == 'C')
-                        {
-                            ram[i*3] += 1;
-                            ram[i*3+numArg] = 9;
-                        }
-                        else
-                        {
-                            j = 1;
-                            if  (argumento[0] >= '0' && argumento[0] <= '9')
-                                ram[i*3+numArg] = atoi(argumento);
-                            else
-                                if (argumento[0] == '#')
-                                {
-                                    argumento[0] = '0';
-                                    ram[i*3+numArg] = atoi(argumento);
-                                }
+                        if (argumento[0] == '@') //Es octal
+                            for(k=strlen(argumento)-2; k>=0; k--)
+                            {
+                                car = argumento[j++] - 48;
+                                ram[i*3+numArg] = (ram[i*3+numArg]<<3) + (car & 0x7);
+                            }
+                        else //Es hexadecimal
+                            for(k=strlen(argumento)-2; k>=0; k--)
+                            {
+                                car = argumento[j++];
+                                if (car <= '9')
+                                    car -= 48;
                                 else
-                                    if (argumento[0] == '\'')
-                                        ram[i*3+numArg] = argumento[1];
-                                    else
-                                        if (argumento[0] == '@')
-                                            for(k=strlen(argumento)-2; k>=0; k--)
-                                            {
-                                                car = argumento[j++] - 48;
-                                                ram[i*3+numArg] = (ram[i*3+numArg]<<3) + (car & 0x7);
-                                            }
-                                        else //Por descarte es %, hexadecimal
-                                            for(k=strlen(argumento)-2; k>=0; k--)
-                                            {
-                                                car = argumento[j++];
-                                                if (car <= '9')
-                                                    car -= 48;
-                                                else
-                                                    car -= 55;
-                                                ram[i*3+numArg] = (ram[i*3+numArg]<<4) + (car & 0xF);
-                                            }
-                        }
+                                    car -= 55;
+                                ram[i*3+numArg] = (ram[i*3+numArg]<<4) + (car & 0xF);
+                            }
+        }
     }
     if (numArg == 1)
         ram[i*3] = ram[i*3] << 8;
 }
 
-long traducirMnemonico(char* instrucciones[], int contInstrucciones, char mnemonico[])
+int traducirRegistro(char* indiceRegistros[], char arg[])
 {
     int i = 0;
-    while (i < contInstrucciones && strcmp(mnemonico,instrucciones[i]) != 0)
+    while (i < 16 && strcmp(arg,indiceRegistros[i]) != 0)
         i++;
-    if (i < contInstrucciones)
+    if (i < 16)
         return i;
     else
         return -1;
