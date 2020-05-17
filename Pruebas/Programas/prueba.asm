8c155cd556e65512e247c831f193a7a4592b90ba
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2020-05-17 10:15:50 -0300

    prueba actualizado

--- Pruebas/Programas/prueba.asm
+++ Pruebas/Programas/prueba.asm
@@ -1,26 +1,33 @@
 
-\\ASM EXTRA =550  	STACK= 700    //Directiva
+\\ASM EXTRA = 550 	 STACK =700    //Directiva
 
 	MOV AX,5
-   cON1   EQU 'A
+	cON1   EQU 'A
 	CoN2 EQU    "Hola como andas"
 	MOV FX ,  	CoN1 //Guarda en FX la constante CON1
 	MOV [ES:AX-1] , %0001 //Prueba si lo indirectos andan bien
 
-SLEN [DS:10], con2 //Guarda la longitud del string CON2 en la celda DS:10
-
+	SLEN [DS:10], con2 //Guarda la longitud del string CON2 en la celda DS:10	
 	MOV AX,[ES:4]
 	MOV CX,1	
 	MOV DX, 10
 	SYS 2 //Imprime la longitud del string CON2
 
 	SMOV [ES:7]  ,  CON2
-	MOV AX,%0100
-	MOV BX,3
-	MOV DX,7
+	SMOV [DS:200],[ES:7]
+	SLEN AX,[DS:200]
+	MOV AX,%0000
+	MOV BX,2
+	MOV DX,200
 	SYS 20 //Imprime la cadena copiada con un endline
+	SYS 0
+	SLEN [DS:10], [DS:200] //Guarda la longitud del string CON2 en la celda DS:10
+	MOV AX,[ES:4]
+	MOV CX,1	
+	MOV DX, 10
+	SYS 2 //Imprime la longitud del string CON2
 
-	SCMP CON2 , [ES:7]
+	SCMP [ES:7], CON2
 	JNZ fin
 
 loop:	MOV AX,0
